0c703ce42aab7c56736f282d897e9bf2402dbb8f
NoFoster <kfoster0@chicagobooth.edu>
2025-04-14 15:58:22 +0000

    Links

--- app.rb
+++ app.rb
@@ -3,9 +3,10 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
+  <h1>Welcome to Rock-Paper-Scissors!</h1>
   <p>Define some routes in app.rb</p>
   "
+
 end
 
 get("/rock") do

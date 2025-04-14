4cd475ffd8ce47b89fa96279d77b6ca18216fe9b
NoFoster <kfoster0@chicagobooth.edu>
2025-04-14 15:50:44 +0000

    paper and scissors base

--- app.rb
+++ app.rb
@@ -23,3 +23,35 @@ end
 
 erb(:rock)
 end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "won"
+  elsif @comp_move = "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
+    erb(:paper)
+  end
+
+
+  get("/scissors") do
+    moves = ["rock", "paper", "scissors"]
+
+    @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "lost"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+
+    erb(:scissors)
+  end

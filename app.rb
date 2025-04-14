826bcc819950daa0f943fa0345aba3d6970820b1
NoFoster <kfoster0@chicagobooth.edu>
2025-04-14 16:07:20 +0000

    Final

--- app.rb
+++ app.rb
@@ -2,10 +2,8 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to Rock-Paper-Scissors!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  erb(:homepage)
 
 end
 
